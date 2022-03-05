7bfc779426bbab025ebb7128e04b674cfb7c4e9f
caleb-freitas <calebfreitas@tutanota.com>
2022-03-05 16:49:15 -0300

    feat: ensure CompanySignUpController return an error if PasswordValidator returns false

--- src/presentation/controllers/company.signup.controller.ts
+++ src/presentation/controllers/company.signup.controller.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from "../errors";
 import { MissingParamError } from "../errors/missing.param.error";
-import { badRequest } from "../helpers/http.helper";
+import { badRequest, serverError } from "../helpers/http.helper";
 import {
   IController,
   IHttpRequest,
@@ -12,27 +12,34 @@ export class CompanySignUpController implements IController {
   constructor(private readonly passwordValidator: IPasswordValidator) {}
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const { password, passwordConfirmation } = httpRequest.body;
-    const requiredFields: string[] = [
-      "name",
-      "email",
-      "country",
-      "cnpj",
-      "password",
-      "passwordConfirmation",
-    ];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const { password, passwordConfirmation } = httpRequest.body;
+      const requiredFields: string[] = [
+        "name",
+        "email",
+        "country",
+        "cnpj",
+        "password",
+        "passwordConfirmation",
+      ];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError("passwordConfirmation"));
+      }
+      const isValid = this.passwordValidator.isValid(password);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("password"));
+      }
+      return {
+        statusCode: 200,
+        body: "",
+      };
+    } catch (error) {
+      return serverError(error);
     }
-    if (password !== passwordConfirmation) {
-      return badRequest(new InvalidParamError("passwordConfirmation"));
-    }
-    this.passwordValidator.isValid(password);
-    return {
-      statusCode: 200,
-      body: "",
-    };
   }
 }
