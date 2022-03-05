a0593e824389580e6e38830661ca4418ac1d9fba
caleb-freitas <calebfreitas@tutanota.com>
2022-03-05 13:02:22 -0300

    refactor: move field validation to flow control

--- src/presentation/controllers/company.signup.controller.ts
+++ src/presentation/controllers/company.signup.controller.ts
@@ -4,23 +4,18 @@ import { IController, IHttpRequest, IHttpResponse } from "../protocols";
 
 export class CompanySignUpController implements IController {
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError("name"));
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError("email"));
-    }
-    if (!httpRequest.body.country) {
-      return badRequest(new MissingParamError("country"));
-    }
-    if (!httpRequest.body.cnpj) {
-      return badRequest(new MissingParamError("cnpj"));
-    }
-    if (!httpRequest.body.password) {
-      return badRequest(new MissingParamError("password"));
-    }
-    if (!httpRequest.body.passwordConfirmation) {
-      return badRequest(new MissingParamError("passwordConfirmation"));
+    const requiredFields: string[] = [
+      "name",
+      "email",
+      "country",
+      "cnpj",
+      "password",
+      "passwordConfirmation",
+    ];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
     return {
       statusCode: 200,
