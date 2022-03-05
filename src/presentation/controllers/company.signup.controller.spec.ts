466866663f3df50283e0b37f358e669c11752fe8
caleb-freitas <calebfreitas@tutanota.com>
2022-03-05 13:23:56 -0300

    test: ensure CompanySignUpController return 400 if invalid passwordConfirmation is provided

--- src/presentation/controllers/company.signup.controller.spec.ts
+++ src/presentation/controllers/company.signup.controller.spec.ts
@@ -1,6 +1,5 @@
-import { MissingParamError } from "../errors/missing.param.error";
+import { InvalidParamError, MissingParamError } from "../errors";
 import { badRequest } from "../helpers/http.helper";
-import { IHttpRequest } from "../protocols";
 import { CompanySignUpController } from "./company.signup.controller";
 
 interface ISutTypes {
@@ -13,95 +12,117 @@ function makeSut(): ISutTypes {
 }
 
 describe("CompanySignUpController", () => {
-  test("should return 400 if no name is provided", async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        email: "valid@mail.com",
-        password: "valid_password",
-        passwordConfirmation: "valid_password",
-        country: "valid_country",
-        cnpj: "valid_cnpj",
-      },
-    };
-    const response = await sut.handle(httpRequest);
-    expect(response).toEqual(badRequest(new MissingParamError("name")));
+  describe("Validate required fields", () => {
+    test("should return 400 if no name is provided", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          email: "valid@mail.com",
+          password: "valid_password",
+          passwordConfirmation: "valid_password",
+          country: "valid_country",
+          cnpj: "valid_cnpj",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(badRequest(new MissingParamError("name")));
+    });
+
+    test("should return 400 if no email is provided", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          name: "valid_name",
+          password: "valid_password",
+          passwordConfirmation: "valid_password",
+          country: "valid_country",
+          cnpj: "valid_cnpj",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(badRequest(new MissingParamError("email")));
+    });
+
+    test("should return 400 if no country is provided", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          name: "valid_name",
+          email: "valid@mail.com",
+          password: "valid_password",
+          passwordConfirmation: "valid_password",
+          cnpj: "valid_cnpj",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(badRequest(new MissingParamError("country")));
+    });
+
+    test("should return 400 if no cnpj is provided", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          name: "valid_name",
+          email: "valid@mail.com",
+          password: "valid_password",
+          passwordConfirmation: "valid_password",
+          country: "valid_country",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(badRequest(new MissingParamError("cnpj")));
+    });
+
+    test("should return 400 if no password is provided", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          name: "valid_name",
+          email: "valid@mail.com",
+          passwordConfirmation: "valid_password",
+          country: "valid_country",
+          cnpj: "valid_cnpj",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(badRequest(new MissingParamError("password")));
+    });
+
+    test("should return 400 if no passwordConfirmation is provided", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          name: "valid_name",
+          email: "valid@mail.com",
+          password: "valid_password",
+          country: "valid_country",
+          cnpj: "valid_cnpj",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(
+        badRequest(new MissingParamError("passwordConfirmation"))
+      );
+    });
   });
 
-  test("should return 400 if no email is provided", async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        name: "valid_name",
-        password: "valid_password",
-        passwordConfirmation: "valid_password",
-        country: "valid_country",
-        cnpj: "valid_cnpj",
-      },
-    };
-    const response = await sut.handle(httpRequest);
-    expect(response).toEqual(badRequest(new MissingParamError("email")));
-  });
-
-  test("should return 400 if no country is provided", async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        name: "valid_name",
-        email: "valid@mail.com",
-        password: "valid_password",
-        passwordConfirmation: "valid_password",
-        cnpj: "valid_cnpj",
-      },
-    };
-    const response = await sut.handle(httpRequest);
-    expect(response).toEqual(badRequest(new MissingParamError("country")));
-  });
-
-  test("should return 400 if no cnpj is provided", async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        name: "valid_name",
-        email: "valid@mail.com",
-        password: "valid_password",
-        passwordConfirmation: "valid_password",
-        country: "valid_country",
-      },
-    };
-    const response = await sut.handle(httpRequest);
-    expect(response).toEqual(badRequest(new MissingParamError("cnpj")));
-  });
-
-  test("should return 400 if no password is provided", async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        name: "valid_name",
-        email: "valid@mail.com",
-        passwordConfirmation: "valid_password",
-        country: "valid_country",
-        cnpj: "valid_cnpj",
-      },
-    };
-    const response = await sut.handle(httpRequest);
-    expect(response).toEqual(badRequest(new MissingParamError("password")));
-  });
-
-  test("should return 400 if no passwordConfirmation is provided", async () => {
-    const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        name: "valid_name",
-        email: "valid@mail.com",
-        password: "valid_password",
-        country: "valid_country",
-        cnpj: "valid_cnpj",
-      },
-    };
-    const response = await sut.handle(httpRequest);
-    expect(response).toEqual(
-      badRequest(new MissingParamError("passwordConfirmation"))
-    );
+  describe("Password  validation", () => {
+    test("should return 400 if password and passwordConfirmation are not equals", async () => {
+      const { sut } = makeSut();
+      const httpRequest = {
+        body: {
+          name: "valid_name",
+          email: "valid@mail.com",
+          password: "valid_password",
+          passwordConfirmation: "invalid_password",
+          country: "valid_country",
+          cnpj: "valid_cnpj",
+        },
+      };
+      const response = await sut.handle(httpRequest);
+      expect(response).toEqual(
+        badRequest(new InvalidParamError("passwordConfirmation"))
+      );
+    });
   });
 });
