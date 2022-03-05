5942c43a5b70d6b496d591be406c98431524f5df
caleb-freitas <calebfreitas@tutanota.com>
2022-03-05 12:12:43 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/company.signup.controller.spec.ts
+++ src/presentation/controllers/company.signup.controller.spec.ts
@@ -1,20 +1,33 @@
 import { MissingParamError } from "../errors/missing.param.error";
 import { badRequest } from "../helpers/http.helper";
+import { IHttpRequest } from "../protocols";
 import { CompanySignUpController } from "./company.signup.controller";
+
+interface ISutTypes {
+  sut: CompanySignUpController;
+}
+
+function makeFakeRequest(): IHttpRequest {
+  return {
+    body: {
+      email: "valid@mail.com",
+      password: "valid_password",
+      passwordConfirmation: "valid_password",
+      country: "valid_country",
+      cnpj: "valid_cnpj",
+    },
+  };
+}
+
+function makeSut(): ISutTypes {
+  const sut = new CompanySignUpController();
+  return { sut };
+}
 
 describe("CompanySignUpController", () => {
   test("should return 400 if no name is provided", async () => {
-    const sut = new CompanySignUpController();
-    const httpRequest = {
-      body: {
-        email: "valid@mail.com",
-        password: "valid_password",
-        passwordConfirmation: "valid_password",
-        country: "valid_country",
-        cnpj: "valid_cnpj",
-      },
-    };
-    const response = await sut.handle(httpRequest);
+    const { sut } = makeSut();
+    const response = await sut.handle(makeFakeRequest());
     expect(response).toEqual(badRequest(new MissingParamError("name")));
   });
 });
