449eff6df18734e316caecb066ae23334fe3b80f
caleb-freitas <calebfreitas@tutanota.com>
2022-03-07 19:02:46 -0300

    feat: ensure CompanySignupController calls EmailValidator with correct email

--- src/presentation/controllers/company.signup.controller.ts
+++ src/presentation/controllers/company.signup.controller.ts
@@ -3,17 +3,21 @@ import { MissingParamError } from "../errors/missing.param.error";
 import { badRequest, serverError } from "../helpers/http.helper";
 import {
   IController,
+  IEmailValidator,
   IHttpRequest,
   IHttpResponse,
   IPasswordValidator,
 } from "../protocols";
 
 export class CompanySignUpController implements IController {
-  constructor(private readonly passwordValidator: IPasswordValidator) {}
+  constructor(
+    private readonly passwordValidator: IPasswordValidator,
+    private readonly emailValidator: IEmailValidator
+  ) {}
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      const { password, passwordConfirmation } = httpRequest.body;
+      const { password, passwordConfirmation, email } = httpRequest.body;
       const requiredFields: string[] = [
         "name",
         "email",
@@ -34,6 +38,7 @@ export class CompanySignUpController implements IController {
       if (!isValid) {
         return badRequest(new InvalidParamError("password"));
       }
+      this.emailValidator.isValid(email);
       return {
         statusCode: 200,
         body: "",
--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,3 +1,5 @@
 export * from "./controller";
 export * from "./http";
 export * from "./password.validator";
+export * from "./email.validator";
+export * from "./cnpj.validator";
