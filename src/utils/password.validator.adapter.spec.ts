9e1e277e25c06cee1373bec4cc28960d436ca434
caleb-freitas <calebfreitas@tutanota.com>
2022-03-07 15:01:51 -0300

    test: ensure PasswordValidatorAdapter validate if password have digits, uppercase, lowercase and minimun 8 characters

--- src/utils/password.validator.adapter.spec.ts
+++ src/utils/password.validator.adapter.spec.ts
@@ -1,14 +1,51 @@
-import { IPasswordValidator } from "../presentation/protocols";
+import { PasswordValidatorAdapter } from "./password.validator.adapter";
 
 describe("PasswordValidatorAdapter", () => {
-  test("should return true if password-validator returns true", () => {
-    class PasswordValidatorAdapter implements IPasswordValidator {
-      isValid(password: string): boolean {
-        return true;
-      }
-    }
+  test("should return true if password have at least 8 characters", () => {
     const sut = new PasswordValidatorAdapter();
-    const response = sut.isValid("valid_password");
+    const response = sut.isValid("ValidPassword123");
     expect(response).toBe(true);
+  });
+
+  test("should return false if password has less than 8 characters", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("invalid12");
+    expect(response).toBe(false);
+  });
+
+  test("should return true if password have uppercase letters", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("ValidPassword123");
+    expect(response).toBe(true);
+  });
+
+  test("should return false if password does not have uppercase letters", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("ValidPassword123");
+    expect(response).toBe(true);
+  });
+
+  test("should return true if password have uppercase letters", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("ValidPassword123");
+    expect(response).toBe(true);
+  });
+
+  test("should return false if password does not have uppercase letters", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("invalid_password123");
+    expect(response).toBe(false);
+  });
+
+  test("should return true if password have digits", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("InvalidPassword123");
+    expect(response).toBe(true);
+  });
+
+  test("should return true if password does not have digits", () => {
+    const sut = new PasswordValidatorAdapter();
+    const response = sut.isValid("InvalidPassword");
+    expect(response).toBe(false);
   });
 });
