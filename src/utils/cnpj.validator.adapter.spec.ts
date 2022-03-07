3af84949273aa3036728f910bebe1bd41e9636ad
caleb-freitas <calebfreitas@tutanota.com>
2022-03-07 18:44:11 -0300

    test: ensure CnpjValidatorAdapter returns true if validator returns true

--- src/utils/cnpj.validator.adapter.spec.ts
+++ src/utils/cnpj.validator.adapter.spec.ts
@@ -9,4 +9,18 @@ describe("CnpjValidatorAdapter", () => {
     sut.isCnpj("25634428777");
     expect(isValidSpy).toHaveBeenCalledWith("25634428777");
   });
+
+  test("should return false if validator return false", () => {
+    const sut = new CnpjValidatorAdapter();
+    jest.spyOn(cnpj, "isValid").mockReturnValueOnce(false);
+    const response = sut.isCnpj("25634428777");
+    expect(response).toBe(false);
+  });
+
+  test("should return true if validator return true", () => {
+    const sut = new CnpjValidatorAdapter();
+    jest.spyOn(cnpj, "isValid").mockReturnValueOnce(true);
+    const response = sut.isCnpj("25634428777");
+    expect(response).toBe(true);
+  });
 });
