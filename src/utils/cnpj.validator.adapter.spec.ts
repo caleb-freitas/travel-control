ee0ef6f810eadece74f02fe3c67ca8e5870383d4
caleb-freitas <caleb.freitas@esmarteng.com>
2022-03-08 11:26:04 -0300

    refactor: move sut creation to a factory method

--- src/utils/cnpj.validator.adapter.spec.ts
+++ src/utils/cnpj.validator.adapter.spec.ts
@@ -1,24 +1,34 @@
 import { cnpj } from "cpf-cnpj-validator";
 
+import { ICnpjValidator } from "../presentation/protocols";
 import { CnpjValidatorAdapter } from "./cnpj.validator.adapter";
+
+interface ISutTypes {
+  sut: ICnpjValidator;
+}
+
+function makeSut(): ISutTypes {
+  const sut = new CnpjValidatorAdapter();
+  return { sut };
+}
 
 describe("CnpjValidatorAdapter", () => {
   test("should call cpf-cnpj-validator with correct cnpj", () => {
-    const sut = new CnpjValidatorAdapter();
+    const { sut } = makeSut();
     const isValidSpy = jest.spyOn(cnpj, "isValid");
     sut.isCnpj("25634428777");
     expect(isValidSpy).toHaveBeenCalledWith("25634428777");
   });
 
   test("should return false if validator return false", () => {
-    const sut = new CnpjValidatorAdapter();
+    const { sut } = makeSut();
     jest.spyOn(cnpj, "isValid").mockReturnValueOnce(false);
     const response = sut.isCnpj("25634428777");
     expect(response).toBe(false);
   });
 
   test("should return true if validator return true", () => {
-    const sut = new CnpjValidatorAdapter();
+    const { sut } = makeSut();
     jest.spyOn(cnpj, "isValid").mockReturnValueOnce(true);
     const response = sut.isCnpj("25634428777");
     expect(response).toBe(true);
