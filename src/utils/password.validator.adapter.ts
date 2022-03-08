1c8bccf5642c5d6a4d3725fdab287e5dfc0f75bc
caleb-freitas <caleb.freitas@esmarteng.com>
2022-03-08 12:05:32 -0300

    refactor: use regexp to validate password

--- src/utils/password.validator.adapter.ts
+++ src/utils/password.validator.adapter.ts
@@ -1,11 +1,8 @@
-import PasswordValidator from "password-validator";
-
 import { IPasswordValidator } from "../presentation/protocols";
 
 export class PasswordValidatorAdapter implements IPasswordValidator {
-  isValid(password: string): boolean | any[] {
-    const schema = new PasswordValidator();
-    schema.is().min(8).has().lowercase().has().uppercase().has().digits();
-    return schema.validate(password);
+  isValid(password: string): boolean {
+    const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return passwordRegExp.test(password);
   }
 }
