81deb6865e5e5ff40b789203f4936dea0f24f55d
caleb-freitas <calebfreitas@tutanota.com>
2022-03-08 19:17:44 -0300

    test: ensure ValidationComposite returns MissingParamError if any validation fails

---
+++ src/validation/validators/validation.composite.spec.ts
@@ -0,0 +1,37 @@
+import { MissingParamError } from "../../presentation/errors";
+import { IValidation } from "../../presentation/protocols/validation";
+import { ValidationComposite } from "./validation.composite";
+
+interface ISutTypes {
+  sut: ValidationComposite;
+  validationsStub: IValidation[];
+}
+
+function makeValidation(): IValidation {
+  class ValidationStub implements IValidation {
+    validate(input: any): Error {
+      return null;
+    }
+  }
+  return new ValidationStub();
+}
+
+function makeSut(): ISutTypes {
+  const validationsStub = [makeValidation(), makeValidation()];
+  const sut = new ValidationComposite(validationsStub);
+  return {
+    sut,
+    validationsStub,
+  };
+}
+
+describe("ValidationComposite", () => {
+  test("should return MissingParamError if any validation fails", () => {
+    const { sut, validationsStub } = makeSut();
+    jest
+      .spyOn(validationsStub[0], "validate")
+      .mockReturnValueOnce(new MissingParamError("field"));
+    const error = sut.validate({ field: "any_field" });
+    expect(error).toEqual(new MissingParamError("field"));
+  });
+});
