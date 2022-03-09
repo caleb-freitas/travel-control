0af1224e8fd51258f7bf829165926457a8a3eeb8
caleb-freitas <caleb.freitas@esmarteng.com>
2022-03-09 20:16:35 -0300

    test: ensure BcryptAdapter throw if bcrypt throw

---
+++ src/infra/cryptography/bcrypt.adapter/bcrypt.adapter.spec.ts
@@ -0,0 +1,36 @@
+import bcrypt from "bcrypt";
+
+import { BcryptAdapter } from "./bcrypt.adapter";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
+const salt = 12;
+
+describe("BcryptAdapter", () => {
+  test("should call bcrypt with correct values", async () => {
+    const sut = new BcryptAdapter(salt);
+    const hashSpy = jest.spyOn(bcrypt, "hash");
+    await sut.hash("any_value");
+    expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("should throw if bcrypt throw", async () => {
+    const sut = new BcryptAdapter(salt);
+    jest.spyOn(bcrypt, "hash").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.hash("any_value");
+    expect(promise).rejects.toThrow();
+  });
+
+  test("should return a valid hash on hash success", async () => {
+    const sut = new BcryptAdapter(salt);
+    const hashSpy = jest.spyOn(bcrypt, "hash");
+    await sut.hash("any_value");
+    expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+});
