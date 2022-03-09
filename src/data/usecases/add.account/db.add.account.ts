6d71e4a460bba583c672a3cee367705792e7aa3e
caleb-freitas <caleb.freitas@esmarteng.com>
2022-03-08 22:46:29 -0300

    feat: ensure DbAddAccount call AddAccountRepository with correct account data

--- src/data/usecases/add.account/db.add.account.ts
+++ src/data/usecases/add.account/db.add.account.ts
@@ -4,11 +4,16 @@ import {
   IAddAccountModel,
 } from "../../../domain/usecases/add.account";
 import { IHasher } from "../../protocols/cryptography/hasher";
+import { IAddAccountRepository } from "../../protocols/database/add.account.repository";
 
 export class DbAddAccount implements IAddAccount {
-  constructor(private readonly hasher: IHasher) {}
+  constructor(
+    private readonly hasher: IHasher,
+    private readonly addAccountRepository: IAddAccountRepository
+  ) {}
   async add(account: IAddAccountModel): Promise<IAccountModel> {
     await this.hasher.hash(account.password);
+    await this.addAccountRepository.add(account);
     return {
       id: "string",
       name: "string",
