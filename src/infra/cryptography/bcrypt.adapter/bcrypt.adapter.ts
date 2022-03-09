1e83f570999d56b6f0eb147fe058e1467dde2fa8
caleb-freitas <caleb.freitas@esmarteng.com>
2022-03-09 20:19:07 -0300

    feat: esnure BcryptAdapter return a valid hash on hash success

--- src/infra/cryptography/bcrypt.adapter/bcrypt.adapter.ts
+++ src/infra/cryptography/bcrypt.adapter/bcrypt.adapter.ts
@@ -6,7 +6,7 @@ export class BcryptAdapter implements IHasher {
   constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return "";
+    const hash = await bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
