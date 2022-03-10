2fc5a5ebba0ac5cd9ddbda034b6346abad753afb
caleb-freitas <caleb.freitas@esmarteng.com>
2022-03-10 20:43:36 -0300

    test: ensure database is clean after integration tests

--- src/infra/database/prisma/account/account.prisma.repository.test.ts
+++ src/infra/database/prisma/account/account.prisma.repository.test.ts
@@ -1,14 +1,25 @@
 import { IAddAccountModel } from "../../../../domain/usecases/add.account";
+import { prisma } from "../prisma.client";
 import { AccountPrismaRepository } from "./account.prisma.repository";
 
+function makeSut(): AccountPrismaRepository {
+  return new AccountPrismaRepository();
+}
+
 describe("AccountPrismaRepository", () => {
+  afterAll(async () => {
+    const deleteCompanies = prisma.company.deleteMany();
+    await prisma.$transaction([deleteCompanies]);
+    await prisma.$disconnect();
+  });
+
   test("should create a new company account", async () => {
-    const sut = new AccountPrismaRepository();
+    const sut = makeSut();
     const account: IAddAccountModel = {
-      name: "Ligeiro Transportes",
-      email: "ligeiro.transportadora@gmail.com",
-      password: "logistica123456",
-      cnpj: "000.000.000/0001-01",
+      name: "company",
+      email: "company@email.com",
+      password: "password",
+      cnpj: "cnpj",
     };
     const response = await sut.add(account);
     expect(response).toHaveProperty("id");
