fb8ecb1afdb5c5db03d4520b1a5af8edb9955c9c
caleb-freitas <calebfreitas@tutanota.com>
2022-03-12 10:40:38 -0300

    test: ensure to call ValidationComposite with all validations

--- src/main/factory/controllers/signup/signup.validation.factory.spec.ts
+++ src/main/factory/controllers/signup/signup.validation.factory.spec.ts
@@ -1,5 +1,13 @@
-import { IEmailValidator } from "../../../../presentation/protocols";
+import { CnpjValidatorAdapter } from "../../../../infra/validators/cnpj.validator.adapter";
+import {
+  IEmailValidator,
+  IPasswordValidator,
+} from "../../../../presentation/protocols";
 import { IValidation } from "../../../../presentation/protocols/validation";
+import {
+  CnpjValidation,
+  PasswordValidation,
+} from "../../../../validation/validators";
 import { CompareFieldsValidation } from "../../../../validation/validators/compare.passwords.validation";
 import { EmailValidation } from "../../../../validation/validators/email.validation";
 import { RequiredFieldValidation } from "../../../../validation/validators/required.fields.validation";
@@ -9,16 +17,25 @@ import { makeSignUpValidation } from "./signup.validation.factory";
 // remove the module's default behavior
 jest.mock("../../../../validation/validators/validation.composite.ts");
 
-const makeEmailValidator = (): IEmailValidator => {
+function makeEmailValidator(): IEmailValidator {
   class EmailValidatorStub implements IEmailValidator {
     isValid(email: string): boolean {
       return true;
     }
   }
   return new EmailValidatorStub();
-};
+}
 
-describe("SignUp Validation Factory", () => {
+function makePasswordValidator(): IPasswordValidator {
+  class PasswordValidatorStub implements IPasswordValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new PasswordValidatorStub();
+}
+
+describe("SignUpValidationFactory", () => {
   test("should call validation composite with all validation", () => {
     makeSignUpValidation();
     const validations: IValidation[] = [];
@@ -35,7 +52,11 @@ describe("SignUp Validation Factory", () => {
     validations.push(
       new CompareFieldsValidation("password", "passwordConfirmation")
     );
+    validations.push(
+      new PasswordValidation("password", makePasswordValidator())
+    );
     validations.push(new EmailValidation("email", makeEmailValidator()));
+    validations.push(new CnpjValidation("cnpj", new CnpjValidatorAdapter()));
     expect(ValidationComposite).toHaveBeenLastCalledWith(validations);
   });
 });
