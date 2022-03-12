f01d2bd619bbd3f14af39a63523cc06201eb60f7
caleb-freitas <calebfreitas@tutanota.com>
2022-03-12 17:56:16 -0300

    refactor: delete data after test run

--- tests/main/routes/signup.routes.test.ts
+++ tests/main/routes/signup.routes.test.ts
@@ -1,16 +1,23 @@
 import request from "supertest";
 
+import { prisma } from "../../../src/infra/database/prisma/prisma.client";
 import app from "../../../src/main/config/app";
 
 describe("POST /api/company/signup", () => {
+  afterAll(async () => {
+    const deleteCompanies = prisma.company.deleteMany();
+    await prisma.$transaction([deleteCompanies]);
+    await prisma.$disconnect();
+  });
+
   test("should return 200 on signup", async () => {
     await request(app)
       .post("/api/company/signup")
       .send({
-        name: "user",
-        email: "user1@mail.com",
-        password: "AnyPassword1234",
-        passwordConfirmation: "AnyPassword1234",
+        name: "Company Name",
+        email: "company@email.com",
+        password: "ValidPassword1234",
+        passwordConfirmation: "ValidPassword1234",
         cnpj: "19.587.837/0001-07",
       })
       .expect(200);
