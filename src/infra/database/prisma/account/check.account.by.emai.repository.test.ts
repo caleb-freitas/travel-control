d6f9d96752116b87aabdb38324a061f7cbd8027c
caleb-freitas <calebfreitas@tutanota.com>
2022-03-12 15:43:10 -0300

    test: ensure CheckAccountByEmailRepository return true if an email was registered

---
+++ src/infra/database/prisma/account/check.account.by.emai.repository.test.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../prisma.client";
+import { AccountPrismaRepository } from "./account.prisma.repository";
+import { CheckAccountByEmailRepository } from "./check.account.by.emai.repository";
+
+function makeAccountPrismaRepository(): AccountPrismaRepository {
+  return new AccountPrismaRepository();
+}
+
+function makeSut(): CheckAccountByEmailRepository {
+  return new CheckAccountByEmailRepository();
+}
+
+describe("CheckAccountByEmailRepository", () => {
+  beforeAll(async () => {
+    const accountPrismaRepository = makeAccountPrismaRepository();
+    await accountPrismaRepository.add({
+      name: "any_name",
+      email: "registered@email.com",
+      password: "ValidPass1234",
+      cnpj: "30.270.488/0001-38",
+    });
+  });
+
+  afterAll(async () => {
+    const deleteCompanies = prisma.company.deleteMany();
+    await prisma.$transaction([deleteCompanies]);
+    await prisma.$disconnect();
+  });
+
+  test("should return true if an email was registered", async () => {
+    const sut = makeSut();
+    const response = await sut.checkEmail("registered@email.com");
+    expect(response).toBe(true);
+  });
+});
