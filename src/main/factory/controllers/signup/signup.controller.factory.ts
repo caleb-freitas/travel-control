3d83c7ecb9b7517dc469c7796d6c321652bd0656
caleb-freitas <calebfreitas@tutanota.com>
2022-03-12 10:42:47 -0300

    refactor: add validation composite

--- src/main/factory/controllers/signup/signup.controller.factory.ts
+++ src/main/factory/controllers/signup/signup.controller.factory.ts
@@ -1,16 +1,11 @@
-import { CnpjValidatorAdapter } from "../../../../infra/validators/cnpj.validator.adapter";
-import { EmailValidatorAdapter } from "../../../../infra/validators/email.validator.adapter";
-import { PasswordValidatorAdapter } from "../../../../infra/validators/password.validator.adapter";
 import { CompanySignUpController } from "../../../../presentation/controllers/company.signup.controller";
 import { IController } from "../../../../presentation/protocols";
 import { makeDbAddAccount } from "../../add.account/db.add.account.factory";
-import {} from "./signup.validation.factory";
+import { makeSignUpValidation } from "./signup.validation.factory";
 
 export const makeSignUpController = (): IController => {
   return new CompanySignUpController(
-    new PasswordValidatorAdapter(),
-    new EmailValidatorAdapter(),
-    new CnpjValidatorAdapter(),
-    makeDbAddAccount()
+    makeDbAddAccount(),
+    makeSignUpValidation()
   );
 };
