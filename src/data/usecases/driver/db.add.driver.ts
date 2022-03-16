20b7c912136a799f0609ae62445eb151335a9950
caleb-freitas <calebfreitas@tutanota.com>
2022-03-16 19:31:06 -0300

    feat: ensure DbAddDriver call AddDriverRepository with correct values

--- src/data/usecases/driver/db.add.driver.ts
+++ src/data/usecases/driver/db.add.driver.ts
@@ -3,12 +3,17 @@ import {
   IAddDriver,
   IAddDriverModel,
 } from "../../../domain/usecases/add.driver";
+import { IAddDriverRepository } from "../../protocols/database/driver/add.driver.repository";
 import { IHasher } from "../company/db.add.company.protocols";
 
 export class DbAddDriver implements IAddDriver {
-  constructor(private readonly hasher: IHasher) {}
+  constructor(
+    private readonly hasher: IHasher,
+    private readonly addDriverRepository: IAddDriverRepository
+  ) {}
   async add(account: IAddDriverModel): Promise<IDriverModel | boolean> {
     await this.hasher.hash(account.password);
+    await this.addDriverRepository.add(account);
     return true;
   }
 }
