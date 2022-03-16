cb18c0f9153be5432d2154f0a4457ed0f7a97011
caleb-freitas <calebfreitas@tutanota.com>
2022-03-16 15:54:47 -0300

    feat: ensure DriverSignUpController return 400 if Validation return an error

--- src/presentation/controllers/signup/driver.signup.controller.ts
+++ src/presentation/controllers/signup/driver.signup.controller.ts
@@ -1,4 +1,4 @@
-import { serverError } from "../../helpers/http.helper";
+import { badRequest, serverError } from "../../helpers/http.helper";
 import { IController, IHttpRequest, IHttpResponse } from "../../protocols";
 import { IValidation } from "../../protocols/validation";
 
@@ -6,7 +6,10 @@ export class DriverSignUpController implements IController {
   constructor(private readonly validation: IValidation) {}
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      this.validation.validate(httpRequest.body);
+      const error = this.validation.validate(httpRequest.body);
+      if (error) {
+        return badRequest(error);
+      }
       return {
         statusCode: 200,
         body: {},
