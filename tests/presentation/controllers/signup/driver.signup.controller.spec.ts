21d477747a0fbf628b8102a590dc241c5716a069
caleb-freitas <calebfreitas@tutanota.com>
2022-03-16 00:35:56 -0300

    refactor: move sut creation to a factory function

--- tests/presentation/controllers/signup/driver.signup.controller.spec.ts
+++ tests/presentation/controllers/signup/driver.signup.controller.spec.ts
@@ -1,33 +1,52 @@
 import { DriverSignUpController } from "../../../../src/presentation/controllers/signup/driver.signup.controller";
+import {
+  IController,
+  IHttpRequest,
+} from "../../../../src/presentation/protocols";
 import { IValidation } from "../../../../src/presentation/protocols/validation";
+
+function makeFakeRequest(): IHttpRequest {
+  return {
+    body: {
+      company_id: "company_id",
+      name: "any_name",
+      email: "any@email.com",
+      password: "any_password",
+      passwordConfirmation: "any_password",
+      driverLicense: "any_driver_license",
+      created_at: "any_date",
+    },
+  };
+}
 
 function makeValidation(): IValidation {
   class ValidationStub implements IValidation {
-    validate(input: any): Error | null {
+    validate(input: any): Error {
       return null;
     }
   }
   return new ValidationStub();
 }
 
-const validationStub = makeValidation();
+interface ISutTypes {
+  sut: IController;
+  validationStub: IValidation;
+}
+
+function makeSut(): ISutTypes {
+  const validationStub = makeValidation();
+  const sut = new DriverSignUpController(validationStub);
+  return {
+    sut,
+    validationStub,
+  };
+}
 
 describe("DriverSignUpController", () => {
   test("should call Validation with correct values", async () => {
-    const sut = new DriverSignUpController(validationStub);
+    const { sut, validationStub } = makeSut();
     const validateSpy = jest.spyOn(validationStub, "validate");
-    const httpRequest = {
-      body: {
-        company_id: "company_id",
-        name: "any_name",
-        email: "any@email.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-        driverLicense: "any_driver_license",
-        created_at: "any_date",
-      },
-    };
-    await sut.handle(httpRequest);
-    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+    await sut.handle(makeFakeRequest());
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body);
   });
 });
