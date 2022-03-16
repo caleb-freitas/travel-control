73864519aa55b4a4bc7da3c6a25f576968f39264
caleb-freitas <calebfreitas@tutanota.com>
2022-03-16 18:51:22 -0300

    test: ensure DbAddDriver call Hasher with correct password

---
+++ tests/data/usecases/driver/db.add.driver.spec.ts
@@ -0,0 +1,41 @@
+import { IHasher } from "../../../../src/data/protocols/cryptography/hasher";
+import { DbAddDriver } from "../../../../src/data/usecases/driver/db.add.driver";
+import { IAddDriver } from "../../../../src/domain/usecases/add.driver";
+
+function makeHasher(): IHasher {
+  class HasherStub implements IHasher {
+    async hash(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_string"));
+    }
+  }
+  return new HasherStub();
+}
+
+interface ISutTypes {
+  sut: IAddDriver;
+  hasherStub: IHasher;
+}
+
+function makeSut(): ISutTypes {
+  const hasherStub = makeHasher();
+  const sut = new DbAddDriver(hasherStub);
+  return {
+    sut,
+    hasherStub,
+  };
+}
+
+describe("DbAddDriver", () => {
+  test("should call Hasher with correct password", async () => {
+    const { sut, hasherStub } = makeSut();
+    const hashSpy = jest.spyOn(hasherStub, "hash");
+    await sut.add({
+      company_id: "valid_id",
+      name: "valid_name",
+      email: "valid@email.com",
+      password: "valid_password",
+      driversLicense: "valid_password",
+    });
+    expect(hashSpy).toHaveBeenCalledWith("valid_password");
+  });
+});
