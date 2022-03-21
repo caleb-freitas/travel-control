66074051f73b335e002f50c0d1d9c2197fd7802a
caleb-freitas <calebfreitas@tutanota.com>
2022-03-21 19:59:55 -0300

    feat: inject CheckDriverByEmailRepository to DbAddDriver factory

--- src/main/factory/usecases/db.add.driver.factory.ts
+++ src/main/factory/usecases/db.add.driver.factory.ts
@@ -1,11 +1,17 @@
 import { DbAddDriver } from "../../../data/usecases/driver/db.add.driver";
 import { IAddDriver } from "../../../domain/usecases/add.driver";
 import { BcryptAdapter } from "../../../infra/cryptography/bcrypt.adapter/bcrypt.adapter";
+import { CheckDriverByEmailRepository } from "../../../infra/database/prisma/driver/check.driver.by.email.repository";
 import { DriverRepository } from "../../../infra/database/prisma/driver/driver.repository";
 
 export function makeDbAddDriver(): IAddDriver {
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
   const driverRepository = new DriverRepository();
-  return new DbAddDriver(bcryptAdapter, driverRepository);
+  const checkDriverByEmailRepository = new CheckDriverByEmailRepository();
+  return new DbAddDriver(
+    bcryptAdapter,
+    driverRepository,
+    checkDriverByEmailRepository
+  );
 }
