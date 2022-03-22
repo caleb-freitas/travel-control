d863d5a3ed18336bdefdb03a9f731936e95877b3
caleb-freitas <calebfreitas@tutanota.com>
2022-03-22 10:48:33 -0300

    feat: add swagger documentation to app

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 
 import middlewareSetup from "./middleware";
 import routesSetup from "./routes";
+import swaggerSetup from "./swagger";
 
 const app = express();
 
+swaggerSetup(app);
 middlewareSetup(app);
 routesSetup(app);
 
