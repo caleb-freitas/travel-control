04b7395e19953cf8655f848df15ffe7616191d1e
caleb-freitas <calebfreitas@tutanota.com>
2022-04-04 15:56:38 -0300

    feat: ensure DbCompanyAuthentication call LoadCompanyByEmailRepository with correct email

--- src/data/usecases/db.company.authentication.ts
+++ src/data/usecases/db.company.authentication.ts
@@ -1,12 +1,18 @@
-import {
-  Authentication,
-  IAuthentication,
-} from "@/domain/usecases/authentication";
+import { Authentication, IAuthentication } from "@/domain/usecases";
+
+import { ILoadCompanyByEmailRepository } from "../protocols";
 
 export class DbCompanyAuthentication implements IAuthentication {
-  auth(
+  constructor(
+    private readonly loadCompanyByEmail: ILoadCompanyByEmailRepository
+  ) {}
+
+  async auth(
     authenticationParams: Authentication.Params
   ): Promise<Authentication.Result> {
-    throw new Error("Method not implemented.");
+    const company = await this.loadCompanyByEmail.loadByEmail(
+      authenticationParams.email
+    );
+    return null;
   }
 }
