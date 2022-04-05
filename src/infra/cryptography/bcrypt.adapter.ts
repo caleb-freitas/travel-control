2caa81d1388f7003fb56464b66ed42b29d1822ea
caleb-freitas <calebfreitas@tutanota.com>
2022-04-05 12:28:23 -0300

    feat: ensure BcryptAdapter retrun true if compare return true

--- src/infra/cryptography/bcrypt.adapter.ts
+++ src/infra/cryptography/bcrypt.adapter.ts
@@ -1,11 +1,13 @@
-import { IHasher } from "@/data/protocols/cryptography/hasher";
+import { IHasher, IHashComparer } from "@/data/protocols/cryptography";
 import bcrypt from "bcrypt";
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
   constructor(private readonly salt: number) {}
 
-  async hash(value: string): Promise<string> {
-    const hash = await bcrypt.hash(value, this.salt);
-    return hash;
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt);
+  }
+  async compare(plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest);
   }
 }
