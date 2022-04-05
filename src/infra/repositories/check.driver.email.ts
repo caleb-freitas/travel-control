e12eb91fb1cea700439c74959ec43e08483dc266
caleb-freitas <calebfreitas@tutanota.com>
2022-04-05 10:46:54 -0300

    style: move prettier line

--- src/infra/repositories/check.driver.email.ts
+++ src/infra/repositories/check.driver.email.ts
@@ -1,9 +1,8 @@
 import { ICheckDriverByEmailRepository } from "@/data/protocols";
 import { prisma } from "@/infra/repositories";
 
-export class CheckDriverByEmailRepository
-  // eslint-disable-next-line prettier/prettier
-  implements ICheckDriverByEmailRepository {
+// eslint-disable-next-line prettier/prettier
+export class CheckDriverByEmailRepository implements ICheckDriverByEmailRepository {
   async checkEmail(email: string): Promise<boolean> {
     const emailExists = await prisma.driver.findFirst({
       where: {
