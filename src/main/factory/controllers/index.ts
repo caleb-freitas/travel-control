2cf31b462be26418b8378c2a9e19125a1f1a8ca4
caleb-freitas <calebfreitas@tutanota.com>
2022-04-06 09:48:19 -0300

    refactor: simplify folder structure

--- src/main/factory/controllers/index.ts
+++ src/main/factory/controllers/index.ts
@@ -1,1 +1,4 @@
-export * from "./signup";
+export * from "./company.signup.controller.factory";
+export * from "./company.signup.validation.factory";
+export * from "./driver.signup.controller.factory";
+export * from "./driver.signup.validation.factory";
