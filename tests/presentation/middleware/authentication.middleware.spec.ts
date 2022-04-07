d642c0c4bec57803a93fe92e34d03c71f15d5868
caleb-freitas <calebfreitas@tutanota.com>
2022-04-07 16:45:40 -0300

    feat: ensure AuthenticationMiddleware return 403 if DbLoadAccountByToken does not return an account

--- tests/presentation/middleware/authentication.middleware.spec.ts
+++ tests/presentation/middleware/authentication.middleware.spec.ts
@@ -1,6 +1,9 @@
 import { ILoadAccountByToken } from "@/domain/usecases";
+import { ServerError, AccessDeniedError } from "@/presentation/errors";
+import { serverError, forbidden } from "@/presentation/helpers";
 import { AuthenticationMiddleware } from "@/presentation/middleware";
 import { IHttpRequest } from "@/presentation/protocols";
+import { throwError } from "@/tests/domain/mocks";
 import { DbLoadAccountByTokenSpy } from "@/tests/presentation/mocks";
 
 function mockRequest(): IHttpRequest {
@@ -31,5 +34,21 @@ describe("AuthenticationMiddleware", () => {
     const loadSpy = jest.spyOn(dbLoadAccountByTokenSpy, "load");
     await sut.handle(mockRequest());
     expect(loadSpy).toHaveBeenCalledWith("any_token");
+  });
+
+  test("should return 500 if DbLoadAccountByToken throw", async () => {
+    const { sut, dbLoadAccountByTokenSpy } = makeSut();
+    jest
+      .spyOn(dbLoadAccountByTokenSpy, "load")
+      .mockImplementationOnce(throwError);
+    const response = await sut.handle(mockRequest());
+    expect(response).toEqual(serverError(new ServerError()));
+  });
+
+  test("should return 403 if DbLoadAccountByToken does not find an access token", async () => {
+    const { sut, dbLoadAccountByTokenSpy } = makeSut();
+    jest.spyOn(dbLoadAccountByTokenSpy, "load").mockImplementationOnce(null);
+    const response = await sut.handle(mockRequest());
+    expect(response).toEqual(forbidden(new AccessDeniedError()));
   });
 });
