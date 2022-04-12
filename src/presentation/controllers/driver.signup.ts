2ba40ed881271052ae9d5c575e6153b3f45e62d8
caleb-freitas <calebfreitas@tutanota.com>
2022-04-12 08:42:32 -0300

    feat: use object destructuring

--- src/presentation/controllers/driver.signup.ts
+++ src/presentation/controllers/driver.signup.ts
@@ -22,18 +22,12 @@ export class DriverSignUpController implements IController {
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      const { company_id, name, email, password, drivers_license } =
-        httpRequest.body;
       const error = this.validation.validate(httpRequest.body);
       if (error) {
         return badRequest(error);
       }
       const account = await this.addDriver.add({
-        company_id,
-        name,
-        email,
-        password,
-        drivers_license,
+        ...httpRequest.body,
       });
       if (account instanceof FieldInUseError) {
         return forbidden(account);
