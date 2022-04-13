f86e8406ad18e84eac784d2a8bc15966ce384b91
caleb-freitas <calebfreitas@tutanota.com>
2022-04-13 12:38:02 -0300

    test: ensure CheckTruckByIdRepository return true if truck exist

---
+++ tests/infra/repositories/check.truck.id.test.ts
@@ -0,0 +1,39 @@
+import {
+  CheckTruckByIdRepository,
+  AddCompanyRepository,
+  AddTruckRepository,
+  prisma,
+} from "@/infra/repositories";
+import { mockCompanyParams, mockTruckParams } from "@/tests/domain/mocks";
+
+function checkTruckIdSut(): CheckTruckByIdRepository {
+  return new CheckTruckByIdRepository();
+}
+
+describe("CheckTruckByIdRepository", () => {
+  afterAll(async () => {
+    const deleteCompanies = prisma.company.deleteMany();
+    const deleteTrucks = prisma.truck.deleteMany();
+    await prisma.$transaction([deleteTrucks, deleteCompanies]);
+    await prisma.$disconnect();
+  });
+
+  test("should return false if a company id does not exists", async () => {
+    const sut = checkTruckIdSut();
+    const response = await sut.check("non-existing_truck_id");
+    expect(response).toBe(false);
+  });
+
+  test("should return true if a company id exists", async () => {
+    const sut = checkTruckIdSut();
+    const companyRepository = new AddCompanyRepository();
+    const truckRepository = new AddTruckRepository();
+    const company = await companyRepository.add(mockCompanyParams());
+    const truck = await truckRepository.add({
+      ...mockTruckParams(),
+      company_id: company.id,
+    });
+    const response = await sut.check(truck.id);
+    expect(response).toBe(true);
+  });
+});
