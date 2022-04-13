517c137d39fa22ce949765631f06a00729026002
caleb-freitas <calebfreitas@tutanota.com>
2022-04-13 12:40:04 -0300

    feat: ensure CheckDriverByIdRepository return true if driver exits

--- src/infra/repositories/driver/index.ts
+++ src/infra/repositories/driver/index.ts
@@ -2,3 +2,4 @@ export * from "./add.driver";
 export * from "./check.driver.email";
 export * from "./load.driver.by.email";
 export * from "./update.driver.token";
+export * from "./check.driver.id";
