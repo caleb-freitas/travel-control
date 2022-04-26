39edf51180cc09d9a232c554bc9e8eddc118a5be
caleb-freitas <calebfreitas@tutanota.com>
2022-04-26 20:21:23 -0300

    feat(infra): add load expenses by travel implementation

---
+++ src/infra/repositories/expense/load.expense.by.travel.ts
@@ -0,0 +1,15 @@
+import { ILoadExpensesByTravelRepository } from "@/data/protocols";
+import { Expense } from "@/domain/usecases";
+import { prisma } from "@/infra/repositories";
+
+// eslint-disable-next-line prettier/prettier
+export class LoadExpenseByTravelRepository implements ILoadExpensesByTravelRepository {
+  async load(travel_id: string): Promise<Expense.Model[]> {
+    const expenses = await prisma.expense.findMany({
+      where: {
+        travel_id,
+      },
+    });
+    return expenses;
+  }
+}
