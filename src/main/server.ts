5ccb8b05cf610ac737ba3ba28a3fa259fec16aca
caleb-freitas <calebfreitas@tutanota.com>
2022-04-25 22:05:46 -0300

    bug(fedora): remove dotenv package from scripts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
-
 import app from "./config/app";
 
-dotenv.config();
-const port = Number(process.env.PORT) || 3000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`app running at port ${port}`);
